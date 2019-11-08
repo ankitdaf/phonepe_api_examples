a3d3c2907c4456e1908945dbb0053af9329b3575
Param Aggarwal <paramaggarwal@gmail.com>
2019-11-08 08:10:24 +0530

    Update phonepe_api.py

--- Python/phonepe_api.py
+++ Python/phonepe_api.py
@@ -21,6 +21,25 @@ def set_environment(ENV):
     BASE_URL = phonepe_constants.URLS[ENV]
 
 
+def make_base64(json_obj):
+    json_str = json.dumps(json_obj, separators=(',', ':'))  # compact encoding
+    return base64.urlsafe_b64encode(bytes(json_str, "utf-8")).decode("utf-8")
+
+
+def make_hash(input_str):
+    m = hashlib.sha256()
+    m.update(input_str.encode())
+    return m.hexdigest()
+
+
+def make_request_body(base64_payload):
+    request_body = {
+        "request": base64_payload
+    }
+    data_json = json.dumps(request_body)
+    return data_json
+
+
 def make_charge_request(amount, transaction_id, mobile_number, salt_key_index):
     request_payload = {
         "amount": amount,  # Amount in paise
@@ -34,25 +53,20 @@ def make_charge_request(amount, transaction_id, mobile_number, salt_key_index):
         "transactionId": transaction_id,
         "message": "Payment for " + transaction_id
     }
-    request = json.dumps(request_payload).replace(' ', '').replace("'", '"')
-    base64_payload = base64.urlsafe_b64encode(bytes(request, "utf-8")).decode("utf-8")
-    request_body = {
-        "request": base64_payload
-    }
-    VERIFICATION_HASH = base64_payload + CHARGE_ENDPOINT + API_KEYS[salt_key_index]
-    m = hashlib.sha256()
-    m.update(VERIFICATION_HASH.encode())
-    X_VERIFY_STRING = m.hexdigest() + "###" + salt_key_index
+
+    base64_payload = make_base64(request_payload)
+    verification_str = base64_payload + \
+        CHARGE_ENDPOINT + API_KEYS[salt_key_index]
+    X_VERIFY = make_hash(verification_str) + "###" + salt_key_index
+
+    url = BASE_URL + CHARGE_ENDPOINT
+    data = make_request_body(base64_payload)
     headers = {
         "Content-Type": "application/json",
-        "X-VERIFY": X_VERIFY_STRING
+        "X-VERIFY": X_VERIFY
     }
-    data_json = json.dumps(request_body)
-    url = BASE_URL + CHARGE_ENDPOINT
-    print(url)
-    print("Headers: " + str(headers))
-    print("Request body: " + request)
-    response = requests.request("POST", url, data=data_json, headers=headers)
+
+    response = requests.request("POST", url, data=data, headers=headers)
     print(response.status_code, response.text)
     return response
 
@@ -68,42 +82,36 @@ def make_qrinit_request(amount, transaction_id, salt_key_index):
         "transactionId": transaction_id,
         "message": "Payment for " + transaction_id
     }
-    request = json.dumps(request_payload).replace(' ', '').replace("'", '"')
-    base64_payload = base64.urlsafe_b64encode(bytes(request, "utf-8")).decode("utf-8")
-    request_body = {
-        "request": base64_payload
-    }
-    VERIFICATION_HASH = base64_payload + QRINIT_ENDPOINT + API_KEYS[salt_key_index]
-    m = hashlib.sha256()
-    m.update(VERIFICATION_HASH.encode())
-    X_VERIFY_STRING = m.hexdigest() + "###" + salt_key_index
+
+    base64_payload = make_base64(request_payload)
+    verification_str = base64_payload + \
+        QRINIT_ENDPOINT + API_KEYS[salt_key_index]
+    X_VERIFY = make_hash(verification_str) + "###" + salt_key_index
+
+    url = BASE_URL + QRINIT_ENDPOINT
+    data = make_request_body(base64_payload)
     headers = {
         "Content-Type": "application/json",
-        "X-VERIFY": X_VERIFY_STRING
+        "X-VERIFY": X_VERIFY
     }
-    data_json = json.dumps(request_body)
-    url = BASE_URL + QRINIT_ENDPOINT
-    print(url)
-    print("Headers: " + str(headers))
-    print("Request body: " + request)
-    response = requests.request("POST", url, data=data_json, headers=headers)
+
+    response = requests.request("POST", url, data=data, headers=headers)
     print(response.status_code, response.text)
     return response
 
 
 def make_status_request(transaction_id, salt_key_index):
-    endpoint = TRANSACTION_ENDPOINT + "/" + MERCHANT_ID + "/" + transaction_id + "/status"
+    endpoint = TRANSACTION_ENDPOINT + "/" + \
+        MERCHANT_ID + "/" + transaction_id + "/status"
+    verification_str = endpoint + API_KEYS[salt_key_index]
+    X_VERIFY = make_hash(verification_str) + "###" + salt_key_index
+
     url = BASE_URL + endpoint
-    VERIFICATION_HASH = endpoint + API_KEYS[salt_key_index]
-    m = hashlib.sha256()
-    m.update(VERIFICATION_HASH.encode())
-    X_VERIFY_STRING = m.hexdigest() + "###" + salt_key_index
     headers = {
         "Content-Type": "application/json",
-        "X-VERIFY": X_VERIFY_STRING
+        "X-VERIFY": X_VERIFY
     }
-    print(url)
-    print("Headers: " + str(headers))
+
     response = requests.request("GET", url, headers=headers)
     print(response.status_code, response.text)
     return response
@@ -111,17 +119,15 @@ def make_status_request(transaction_id, salt_key_index):
 
 def make_cancel_request(transaction_id, salt_key_index):
     endpoint = CHARGE_ENDPOINT + "/" + MERCHANT_ID + "/" + transaction_id + "/cancel"
+    verification_str = endpoint + API_KEYS[salt_key_index]
+    X_VERIFY = make_hash(verification_str) + "###" + salt_key_index
+
     url = BASE_URL + endpoint
-    VERIFICATION_HASH = endpoint + API_KEYS[salt_key_index]
-    m = hashlib.sha256()
-    m.update(VERIFICATION_HASH.encode())
-    X_VERIFY_STRING = m.hexdigest() + "###" + salt_key_index
     headers = {
         "Content-Type": "application/json",
-        "X-VERIFY": X_VERIFY_STRING
+        "X-VERIFY": X_VERIFY
     }
-    print(url)
-    print("Headers: " + str(headers))
+
     response = requests.request("POST", url, headers=headers)
     print(response.status_code, response.text)
     return response
@@ -135,24 +141,19 @@ def make_refund_request(transaction_id, provider_reference_id, salt_key_index):
         "transactionId": transaction_id + "_refund",
         "message": "Refund"
     }
-    request = json.dumps(request_payload).replace(' ', '').replace("'", '"')
-    base64_payload = base64.urlsafe_b64encode(bytes(request, "utf-8")).decode("utf-8")
-    request_body = {
-        "request": base64_payload
-    }
-    VERIFICATION_HASH = base64_payload + REFUND_ENDPOINT + API_KEYS[salt_key_index]
-    m = hashlib.sha256()
-    m.update(VERIFICATION_HASH.encode())
-    X_VERIFY_STRING = m.hexdigest() + "###" + salt_key_index
+
+    base64_payload = make_base64(request_payload)
+    verification_str = base64_payload + \
+        REFUND_ENDPOINT + API_KEYS[salt_key_index]
+    X_VERIFY = make_hash(verification_str) + "###" + salt_key_index
+
+    url = BASE_URL + REFUND_ENDPOINT
+    data = make_request_body(base64_payload)
     headers = {
         "Content-Type": "application/json",
-        "X-VERIFY": X_VERIFY_STRING
+        "X-VERIFY": X_VERIFY
     }
-    data_json = json.dumps(request_body)
-    url = BASE_URL + REFUND_ENDPOINT
-    print(url)
-    print("Headers: " + str(headers))
-    print("Request body:" + request)
-    response = requests.request("POST", url, data=data_json, headers=headers)
+
+    response = requests.request("POST", url, data=data, headers=headers)
     print(response.status_code, response.text)
     return response
